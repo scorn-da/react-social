9679563b9875fb0734f91738ae1aa5805051532f
Дмитрий Варганов <varganovda@gmail.com>
2022-12-13 08:33:56 +0300

    fix(src/redux): fixed the redux usage

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -2,8 +2,8 @@ import { combineReducers, createStore } from "redux";
 import profileReducer from "src/redux/profileReducer";
 import dialogsReducer from "src/redux/dialogsReducer";
 
-const reducers = combineReducers({ profileReducer, dialogsReducer });
+const reducers = combineReducers({ profilePage: profileReducer, dialogsPage: dialogsReducer });
 
-const store = createStore();
+const store = createStore(reducers);
 
 export default store;
